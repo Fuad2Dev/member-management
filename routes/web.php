8d5f74ef1b67c45fcb15f572fa9e36efc12b66d5
Fuad-Muhammed <94391454+Fuad2Dev@users.noreply.github.com>
2023-01-23 16:29:57 +0000

    create member resource controller & route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MemberController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -31,3 +32,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+Route::resource('member', MemberController::class);
